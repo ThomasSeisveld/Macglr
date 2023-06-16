e0c48b45bb3006e6a8f6b18351b5071c45381130
ThomasSeisveld <122461596+ThomasSeisveld@users.noreply.github.com>
2023-06-16 15:19:35 +0200

    ww

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Villas4U</title>
+    <link rel="stylesheet" href="CSS/">
+    <link rel="stylesheet" href="CSS/">
+    <link rel="stylesheet" href="CSS/">
+</head>
+<body>
+<nav class="navbar"></nav>
+<header class="hero-section">
+    <div class="content">
+        <img src="img/logo2.jpg" class="logo" alt="">
+        <p class="sub-heading">Beste Nederlandse Villa's</p>
+    </div>
+</header>
+<div class="container">
+    <div class="flexbox">
+        <div1>
+            <a href="house.php?id=huis1"><img src="img/huis1.jpeg" alt="huis" class="huis-img"></a>
+            <h4 class="huis" style="color: #2AA2D6;">Zijdeweg 26</h4>
+            <p class="huis">2244 BG Wassenaar</p>
+            <p class="huis"><b>€ 1.365.000</b></p>
+        </div1>
+        <div1>
+            <a href="house.php?id=huis2"><img src="img/huis2.jpeg" alt="huis" class="huis-img"></a>
+            <h4 class="huis" style="color: #2AA2D6;">Botersloot 545</h4>
+            <p class="huis">3011 HE Rotterdam</p>
+            <p class="huis"><b>€ 1.570.000</b></p>
+        </div1>
+        <div1>
+            <a href="house.php?id=huis3"><img src="img/huis3.jpeg" alt="huis" class="huis-img"></a>
+            <h4 class="huis" style="color: #2AA2D6;">Botersloot 547</h4>
+            <p class="huis">3011 HE Rotterdam</p>
+            <p class="huis"><b>€ 1.975.000</b></p>
+        </div1>
+        <div1>
+            <a href="house.php?id=huis4"><img src="img/huis4.jpeg" alt="huis" class="huis-img"></a>
+            <h4 class="huis" style="color: #2AA2D6;">Wijnhaven 73 X</h4>
+            <p class="huis">2244 WK Rotterdam</p>
+            <p class="huis"><b>€ 1.850.000 </b></p>
+        </div1>
+    </div>
+</div>
+
+<div class="container">
+    <img src="img/view-hr.jpg" alt="view" id="viewHr">
+    <div class="centered">Informatie</div>
+</div>
+
+
+<div class="main-info">
+    <h2><b>Welkom bij onze website voor de verkoop van huizen!</b></h2>
+    <br>
+    <p>Bij ons vindt u een uitgebreid aanbod van prachtige woningen die te koop staan. Of u nu op
+        zoek bent naar een gezinswoning, een appartement, een villa of een ander type woning, wij
+        hebben voor elk wat wils. Ons ervaren team van makelaars staat klaar om u te begeleiden
+        bij het vinden van uw droomhuis.
+        <br><br>
+        Elke woning op onze website wordt zorgvuldig gepresenteerd met gedetailleerde informatie en foto's. U kunt eenvoudig door ons aanbod bladeren en de huizen filteren op basis van uw specifieke criteria, zoals locatie, prijsklasse, aantal slaapkamers en nog veel meer.
+        <br><br>
+        Wanneer u een woning vindt die uw interesse wekt, kunt u direct contact met ons opnemen om een bezichtiging te regelen. Onze makelaars helpen u graag verder en beantwoorden al uw vragen met betrekking tot de woning, het koopproces en de buurt.
+        <br><br>
+        Wij begrijpen dat het kopen van een huis een belangrijke beslissing is. Daarom streven we ernaar om u een soepele en transparante ervaring te bieden. Ons team begeleidt u tijdens elke stap van het proces, van het vinden van de juiste woning tot en met de uiteindelijke aankoop.
+        <br><br>
+        Neem vandaag nog een kijkje op onze website en ontdek het brede scala aan beschikbare huizen. We hopen u binnenkort te mogen verwelkomen en u te helpen bij het verwezenlijken van uw woondromen.</p>
+</div>
+
+
+<div class="footer">
+    <hr class="hr-top">
+    <img src="img/logo.png" id="logo-footer" alt="logo">
+    <div class="footer-text">
+        <a href="privacy.php?id=conditions">Conditions</a>
+        <a href="privacy.php?id=cookies">Cookies</a>
+
+        &copy2023Villa4u
+
+        <a href="privacy.php?id=privacy">Privacy</a>
+        <a href="contact.html">Contact</a>
+    </div>
+
+    <hr class="hr-bottom">
+</div>
+<script src="JS/Dev.js"></script>
+</body>
+</html>
